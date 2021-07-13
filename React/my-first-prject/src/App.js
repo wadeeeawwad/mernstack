0bd1c790fa304d953817dce87a5f87c654ed42c4
wadeeeawwad <82997200+wadeeeawwad@users.noreply.github.com>
2021-07-13 11:30:42 +0300

    test

---
+++ React/my-first-prject/src/App.js
@@ -0,0 +1,32 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    
+
+
+    <div className="App">
+      <header className="App-header">
+      
+       
+        <h1>hello Dojo!</h1>
+      
+        <h2>Things I need to do : </h2>
+        <ul >
+        <li>Learn React</li>
+        <li>Climb Mt. Everest</li>
+        <li>Run a marathon</li>
+        <li>Feed the Dogs</li>
+      </ul>   
+      </header>
+      
+    </div>
+    
+  );
+  
+}
+
+
+export default App;
+
